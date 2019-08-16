edac455ff5bd49370e4e11315bcd8376e9f3fc54
Sebastian Gilits <sep.gil@gmail.com>
2019-08-16 22:02:35 +0200

    Extract twitter stream logic into own file.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
 	"github.com/mitchellh/colorstring"
 	"os"
 	"os/signal"
@@ -35,40 +34,22 @@ func main() {
 		printError(errors.New("Please set the client key, secret, access token & secrent"))
 	}
 
-	config := oauth1.NewConfig(client_key, client_secret)
-	token := oauth1.NewToken(access_token, access_secret)
+	stream := NewStream(client_key, client_secret, access_token, access_secret)
 
-	httpClient := config.Client(oauth1.NoContext, token)
+	stream.FilterKeyword("cats")
+	stream.FilterKeyword("dogs")
 
-	client := twitter.NewClient(httpClient)
-
-	demux := twitter.NewSwitchDemux()
-
-	demux.Tweet = func(tweet *twitter.Tweet) {
+	stream.OnTweet(func(tweet *twitter.Tweet) {
 		url := fmt.Sprintf("https://twitter.com/%v/status/%v", tweet.User.ScreenName, tweet.IDStr)
 		fmt.Printf("\n\n%v:\n%v", url, tweet.Text)
+	})
 
-		media_url := ""
-		if len(tweet.Entities.Media) > 0 {
-			media_url = tweet.Entities.Media[0].URL
-			fmt.Printf("\n- Media: %v", media_url)
-		}
-	}
-
-	filters := &twitter.StreamFilterParams{Track: []string{"kitten", "cat", "puppy", "dogs"}}
-
-	stream, err := client.Streams.Filter(filters)
-
-	if err != nil {
-		printError(err)
-	}
-	go demux.HandleChan(stream.Messages)
-
+	stream.Run()
+	defer stream.Stop()
 	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
 	ch := make(chan os.Signal)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	fmt.Println(<-ch)
 
 	fmt.Println("Stopping Stream...")
-	stream.Stop()
 }
---
+++ twitter.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+// Stream represents the twitter stream client containing the filter params,
+// and the actual twitter client
+type Stream struct {
+	client  *twitter.Client
+	stream  *twitter.Stream
+	filters *twitter.StreamFilterParams
+	demux   twitter.SwitchDemux
+}
+
+// NewStream creates a new stream object.
+func NewStream(clientKey string, clientSecret string, accessKey string, accessToken string) *Stream {
+	config := oauth1.NewConfig(client_key, client_secret)
+	token := oauth1.NewToken(access_token, access_secret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	stream := &Stream{
+		client: client,
+		demux:  twitter.NewSwitchDemux(),
+	}
+
+	return stream
+}
+
+// FilterKeyword filters by the given keyword.
+func (stream *Stream) FilterKeyword(keyword string) {
+	if stream.filters == nil {
+		stream.filters = &twitter.StreamFilterParams{
+			Track: []string{keyword},
+		}
+		return
+	}
+
+	if stream.filters.Track == nil {
+		stream.filters.Track = []string{keyword}
+		return
+	}
+
+	stream.filters.Track = append(stream.filters.Track, keyword)
+}
+
+func (stream *Stream) OnTweet(handler func(tweet *twitter.Tweet)) {
+	stream.demux.Tweet = handler
+}
+
+func (stream *Stream) Run() {
+	stream.stream, _ = stream.client.Streams.Filter(stream.filters)
+	go stream.demux.HandleChan(stream.stream.Messages);
+}
+
+func (stream *Stream) Stop() {
+	stream.stream.Stop()
+}
