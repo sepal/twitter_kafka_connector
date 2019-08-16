0b39ef7438e775268b78ee2839eb555ce525c43e
Sebastian Gilits <sep.gil@gmail.com>
2019-08-16 18:41:50 +0200

    Implement basic stream reading.

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 	"github.com/mitchellh/colorstring"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 var (
@@ -30,4 +34,41 @@ func main() {
 	if client_key == "" || client_secret == "" || access_token == "" || access_secret == "" {
 		printError(errors.New("Please set the client key, secret, access token & secrent"))
 	}
+
+	config := oauth1.NewConfig(client_key, client_secret)
+	token := oauth1.NewToken(access_token, access_secret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	demux := twitter.NewSwitchDemux()
+
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		url := fmt.Sprintf("https://twitter.com/%v/status/%v", tweet.User.ScreenName, tweet.IDStr)
+		fmt.Printf("\n\n%v:\n%v", url, tweet.Text)
+
+		media_url := ""
+		if len(tweet.Entities.Media) > 0 {
+			media_url = tweet.Entities.Media[0].URL
+			fmt.Printf("\n- Media: %v", media_url)
+		}
+	}
+
+	filters := &twitter.StreamFilterParams{Track: []string{"kitten", "cat", "puppy", "dogs"}}
+
+	stream, err := client.Streams.Filter(filters)
+
+	if err != nil {
+		printError(err)
+	}
+	go demux.HandleChan(stream.Messages)
+
+	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println(<-ch)
+
+	fmt.Println("Stopping Stream...")
+	stream.Stop()
 }
